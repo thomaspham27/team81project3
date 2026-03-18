9cbc6f0ccf13ecd3bcc461bbbc552b9ba0f4ddc9
tommy <tomp@tamu.edu>
2026-03-18 15:22:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-﻿console.log('Server is running');
+﻿const http = require('http');
+
+// Render provides the PORT environment variable automatically
+const port = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Team 81 Project is Live!');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening on port ${port}`);
+});
